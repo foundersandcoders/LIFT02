ac8dd8adffc2986516c012fe5afc474ad1e5a4a0
Alexander <alex@alexvoiceover.com>
2025-05-29 11:13:26 +0100

    fix: removed old references to second online environment

--- supabase/scripts/push-dev.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-export SUPABASE_DNS_RESOLVER=native
-source ./supabase/dev.env
-supabase db push --db-url "$SUPABASE_DB_URL" --debug
-
-
-echo "Database schema pushed to Supabase successfully."
