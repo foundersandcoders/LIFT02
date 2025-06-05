34928e0dd3dfcebc5ee560a759498c309b4a1e82
Jason Warren <jason@foundersandcoders.com>
2025-06-05 16:42:53 +0100

    feat(types): :sparkles: add Input type and related interfaces for Profile, Question, Response, Action, and Share

---
+++ src/lib/types/inputs.ts
@@ -0,0 +1,21 @@
+export type Input = Profile | Question | Response | Action | Share;
+
+export interface Profile {
+  name: string
+}
+
+export interface Question {
+  name: string
+}
+
+export interface Response {
+  name: string
+}
+
+export interface Action {
+  name: string
+}
+
+export interface Share {
+  name: string
+}
