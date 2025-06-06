2756e5133ee79bea5e3fb1fb89bbc3d55bec69cd
Jaz-spec <jaz.edna@proton.me>
2025-06-06 17:09:29 +0100

    skeleton for displaying question data in place

--- src/lib/components/QuestionCard.svelte
+++ src/lib/components/QuestionCard.svelte
@@ -1,79 +1,74 @@
 <script lang="ts">
+	import { getQuestionById } from '$lib/services/database';
+
 	interface Props {
-		sectionHeader: string;
-		questionText: string;
-		responseValue?: string;
-		actionsValue?: string;
-		questionId?: string | number;
-		onsubmit?: (data: {
-			questionId: string | number | undefined;
-			response: string;
-			actions: string;
-		}) => void;
-		onskip?: (data: { questionId: string | number | undefined }) => void;
+		questionId: string;
 	}
 
-	let {
-		sectionHeader = 'Feelings',
-		questionText = 'How are you today?',
-		responseValue = '',
-		actionsValue = '',
-		questionId = 1,
-		onsubmit = () => {
-			console.log('submit clicked');
-		},
-		onskip = () => {
-			console.log('skip clicked');
-		}
-	}: Props = $props();
+	interface QuestionData {
+		data: {
+			id: string;
+			category: string;
+			question_text: string;
+			order: number;
+		} | null;
+		error: null | Error;
+	}
+
+	let { questionId }: Props = $props();
+
+	let responseValue = '';
+	let actionsValue = '';
 
 	let responseInput = $state(responseValue);
 	let actionsInput = $state(actionsValue);
 
-	function handleSubmit() {
-		onsubmit?.({
-			questionId,
-			response: responseInput,
-			actions: actionsInput
-		});
-	}
-
-	function handleSkip() {
-		onskip?.({
-			questionId
-		});
-	}
+	const getQuestion = async (): QuestionData => {
+		const question = await getQuestionById(questionId);
+		if (question) {
+			console.log('=== QUESTION ===');
+			console.log(question);
+			return question;
+		}
+		return {
+			data: null,
+			error: new Error('Question not found')
+		};
+	};
+	getQuestion();
 </script>
 
-<div class=" m-auto flex min-h-[90dvh] w-sm flex-col justify-around rounded-3xl p-5 shadow-2xl">
-	<header>
-		<h1 class="text-center text-2xl">{sectionHeader}</h1>
-	</header>
+{#await getQuestion() then question}
+	<div class=" m-auto flex min-h-[90dvh] w-sm flex-col justify-around rounded-3xl p-5 shadow-2xl">
+		<header>
+			<h1 class="text-center text-2xl">{question.data.category}</h1>
+		</header>
 
-	<div class="flex flex-col">
-		<label for="response-{questionId}" class="">{questionText}</label>
-		<textarea
-			id="response-{questionId}"
-			bind:value={responseInput}
-			placeholder="Enter your response here..."
-			rows="4"
-			class="mb-2 rounded-2xl border-2 border-[var(--pink)] p-2 outline-none focus:border-[var(--teal)]"
-		></textarea>
-	</div>
+		<div class="flex flex-col">
+			<label for="response-{questionId}" class="">{question.data.question_text}</label>
+			<textarea
+				id="response-{questionId}"
+				bind:value={responseInput}
+				placeholder="Enter your response here..."
+				rows="4"
+				class="mb-2 rounded-2xl border-2 border-[var(--pink)] p-2 outline-none focus:border-[var(--teal)]"
+			></textarea>
+		</div>
 
-	<div class="flex flex-col">
-		<label for="actions-{questionId}">Actions needed:</label>
-		<textarea
-			id="actions-{questionId}"
-			bind:value={actionsInput}
-			placeholder="Enter your response here..."
-			rows="3"
-			class="rounded-2xl border-2 border-[var(--pink)] p-2 outline-none focus:border-[var(--teal)]"
-		></textarea>
-	</div>
+		<div class="flex flex-col">
+			<label for="actions-{questionId}">Actions needed:</label>
+			<textarea
+				id="actions-{questionId}"
+				bind:value={actionsInput}
+				placeholder="Enter your response here..."
+				rows="3"
+				class="rounded-2xl border-2 border-[var(--pink)] p-2 outline-none focus:border-[var(--teal)]"
+			></textarea>
+		</div>
 
-	<div class="flex justify-around">
-		<button type="button" onclick={handleSkip} class="rounded-2xl border p-4">Skip</button>
-		<button type="button" onclick={handleSubmit} class="rounded-2xl border p-4">Submit</button>
+		<div class="flex justify-around">
+			<button type="button" onclick={handleSkip} class="rounded-2xl border p-4">Skip</button>
+			<button type="button" onclick={handleSubmit} class="rounded-2xl border p-4">Submit</button>
+		</div>
 	</div>
-</div>
+{/await}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import '$lib/components/QuestionCard.svelte';
 	import QuestionCard from '$lib/components/QuestionCard.svelte';
+	import { getQuestionById, getQuestions } from '$lib/services/database/questions';
+
+	const questionId = '8b771be1-c38a-4242-8287-ec760cc96b6e';
 </script>
 
-<QuestionCard />
+<QuestionCard {questionId} />
