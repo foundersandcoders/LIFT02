1636f79d76b3bfca70f13162b2179929d77dd2d2
Jason Warren <jason@foundersandcoders.com>
2025-06-07 19:16:29 +0100

    feat(data): :lipstick: query actions with user ID & render action tile

--- src/lib/components/view/dash.svelte
+++ src/lib/components/view/dash.svelte
@@ -1,9 +1,40 @@
 <script lang="ts">
 	import { setView } from "$lib/components/logic/view.svelte";
 	import { setListNum } from "$lib/components/logic/dev.svelte";
+	import { getUserActions } from "$lib/services/database/actions";
 	import { getQuestions } from "$lib/services/database/questions";
+	import type { ActionCheck } from "$lib/types/ui";
 
-	let query = $state(getQuestions());
+	let devMode = $state(true);
+	const toggleDevMode = () => { devMode = !devMode };
+
+	let actions = $state({ exist: false, count: 0 });
+	let queryQuestions = $state(getQuestions());
+
+	let profileId = $derived(devMode ? "550e8400-e29b-41d4-a716-446655440001" : "");
+	let queryActions = $derived(profileId != "" ? getUserActions(profileId) : null);
+
+	async function countActions(profileId:string):Promise<ActionCheck> {
+		let actionsNum = 0;
+		let isError = false;
+
+		const { data, error } = await getUserActions(profileId);
+
+		if (error) {
+			console.error(error);
+			isError = true;
+		} else if (data) {
+			actionsNum = data.length;
+		}
+
+		!isError && actionsNum > 0 ? actions.exist = true : actions.exist = false;
+
+		return {
+			isError: isError,
+			exist: !isError && actionsNum > 0,
+			count: actionsNum
+		};
+	}
 
 	function getCategories(questions: any) {
 		interface Question {
@@ -35,21 +66,53 @@
 </script>
 
 <div class="dev">
-	<h1>view/dash</h1>
+	<div id="dash-header" class="dev">
+		<h1>view/dash</h1>
+		
+		<button
+			class="dev {devMode ? 'button' : 'inactive'}"
+			onclick={() => toggleDevMode()}
+		>
+			<p>Turn {devMode ? 'Off' : 'On'} Dev Mode</p>
+		</button>
+	</div>
 
-	{#await query}
-		<p>Loading...</p>
-	{:then data}
-		{#if data.data}
-			{#each getCategories(data.data) as category}
+	<div id="dev-info" class="dev">
+		<p>Dev Mode: {devMode}</p>
+		<p>Profile ID: {profileId}</p>
+	</div>
+
+	<div id="dash-tiles" class="dev">
+		{#await queryActions}
+			<p>Loading...</p>
+		{:then result}
+			{#if result && result.data}
 				<button class="dev tile" onclick={() => onclick(1)}>
-					<p>{category}</p>
+					<p>{result.data.length} Actions</p>
 				</button>
-			{/each}
-		{:else}
-			<p>No categories found</p>
-		{/if}
-	{:catch error}
-		<p>Error: {error.message}</p>
-	{/await}
+			{:else}
+				<button class="dev inactive" onclick={() => onclick(1)}>
+					<p>0 Actions</p>
+				</button>
+			{/if}
+		{:catch error}
+			<p>Error Getting Actions: {error.message}</p>
+		{/await}
+
+		{#await queryQuestions}
+			<p>Loading...</p>
+		{:then result}
+			{#if result.data}
+				{#each getCategories(result.data) as category}
+					<button class="dev tile" onclick={() => onclick(1)}>
+						<p>{category}</p>
+					</button>
+				{/each}
+			{:else}
+				<p>No categories found</p>
+			{/if}
+		{:catch error}
+			<p>Error Getting Questions: {error.message}</p>
+		{/await}
+	</div>
 </div>
