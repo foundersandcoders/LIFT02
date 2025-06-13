d501814088c762473e1d55ac784bd8f4cc9b8f6e
Jason Warren <jason@foundersandcoders.com>
2025-06-13 18:27:12 +0100

    feat(state): pass state into list questions

--- src/lib/components/cards/ListItem.svelte
+++ src/lib/components/cards/ListItem.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { getContext } from 'svelte';
-	import type { AppState, RowId, TableName, ViewName } from "$lib/types/appState";
+	import type { AppState, Detail, RowId, TableName, ViewName } from "$lib/types/appState";
 	import type { Action, Question } from "$lib/types/tableMain";
 
 	// App State
@@ -8,48 +8,63 @@
 	const app = $derived(getApp());
 
 	// Component Props
-	let {item, table} = $props<{item:Action | Question, table:TableName}>();
+	let {item, table} = $props<{item:Action|Question, table:TableName}>();
 
 	// Context Pulls
+	const setDetail = getContext<(detail:Detail) => void>('setDetail');
 	const setDetailItemId = getContext<(dbId:RowId) => void>('setDetailItem');
 	const setDetailTable = getContext<(table:TableName) => void>('setDetailItem');
 	const setViewName = getContext<(view:ViewName) => void>('setViewName');
 
-	// Event Handlers
-	const onclick = () => {
+	const onclick = (table:null|TableName, item:null|Action|Question) => {
 		setViewName("detail");
-		const event = new CustomEvent('itemClick', {
-			detail: { table, item: { id: item.id } }
+		setDetail({
+			table: table,
+			item: {
+				rowId: item ? (item.id || null) : null
+			}
 		});
-		dispatchEvent(event);
+		// if (dTable == "questions" && dItem.id) {
+		// 	setDetailTable(dTable);
+		// 	setDetailItemId(dItem.id);
+		// };
 	};
 
-	const onListClick = (dTable:TableName, dItem:Action | Question) => {
-		setViewName("detail");
-		if (dTable == "questions" && dItem.id) {
-			setDetailTable(dTable);
-			setDetailItemId(dItem.id);
-		};
-	};
+	// Event Handlers
+	// const onclick = () => {
+	// 	setViewName("detail");
+	// 	const event = new CustomEvent('itemClick', {
+	// 		detail: {
+	// 			table,
+	// 			item: {
+	// 				id: item.id
+	// 			}
+	// 		}
+	// 	});
+	// 	dispatchEvent(event);
+	// };
 
 	const onkeydown = (press:KeyboardEvent) => {
-		if (press.key === 'Enter') onclick();
+		if (press.key === 'Enter') onclick(table, item);
 	};
 </script>
 
-<div role="button" {onclick} {onkeydown} class="dev dev-div flex flex-row justify-between" tabindex="0">
-	<div class="dev dev-div dev-text">
+<button onclick={() => onclick(table, item)} {onkeydown} tabindex="0" class="dev dev-div flex flex-row justify-between">
+	<div id="list-item-status" class="dev dev-div dev-text">
 		<pre>status</pre>
 	</div>
 
-	<div class="dev dev-div dev-text">
-		{#if app.detail.table == "actions"}
+	<div id="list-item-title" class="dev dev-div dev-text">
+		{#if table == "actions"}
+			{@const item = {id: null}}
 			<p>Coming soon, you fuck</p>
-		{:else if item}
-			<p>{item}</p>
+		{:else if table == "questions" && item}
+			<p>{item.preview}</p>
 		{:else}
+			{@const table = null}
+			{@const item = {id: null}}
 			<p>No idea how you got here</p>
 		{/if}
 	</div>
 	<div class="dev dev-div dev-text"><pre>actions</pre></div>
-</div>
+</button>
