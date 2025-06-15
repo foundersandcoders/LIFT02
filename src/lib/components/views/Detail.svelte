2be13428200a873ab5a8b3cd5450a3704fe2f729
Alexander <alex@alexvoiceover.com>
2025-06-15 13:56:57 +0100

    feat: added conditional logic for rendering

--- src/lib/components/views/Detail.svelte
+++ src/lib/components/views/Detail.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
-	import type { View } from '$lib/types/ui';
+	import type { View, Detail } from '$lib/types/ui';
 	import { getContext } from 'svelte';
 	import QuestionCard from '$lib/components/cards/QuestionCard.svelte';
 
 	const setView = getContext<(view: View) => void>('setView');
+
+	const getDetail = getContext<() => Detail>('getDetail');
+	let detail = $derived(getDetail());
 
 	const onBackClick = () => {
 		setView('list');
@@ -11,10 +14,20 @@
 	const questionId = 'bacc6ffa-b589-4bdc-8eb8-d29eeef7f153';
 </script>
 
-<div class="p-0 bg-base-200">
-	<button onclick={onBackClick} class="btn btn-primary ml-4 mt-2 text-primary-content"> Back </button>
+<div class="bg-base-200 p-0">
+	<button onclick={onBackClick} class="btn btn-primary text-primary-content mt-2 ml-4">
+		Back
+	</button>
 
 	<div id="detail-content" class="">
-		<QuestionCard {questionId} />
+		{#if detail == 'response'}
+			<QuestionCard {questionId} />
+		{:else if detail == 'action'}
+			<pre>Action To Do</pre>
+		{:else if detail == 'email'}
+			<pre>Email Preview To Do</pre>
+		{:else}
+			<pre>No Content Loaded</pre>
+		{/if}
 	</div>
 </div>
