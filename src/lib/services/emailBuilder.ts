179943bd766c134b92472a857a0c88faeb999b2e
Alexander <alex@alexvoiceover.com>
2025-06-15 14:22:35 +0100

    feat: created basic generateEmailPreview function

---
+++ src/lib/services/emailBuilder.ts
@@ -0,0 +1,13 @@
+export function generateEmailPreview(): string {
+	return `Subject: My Workplace Passport
+
+  Hello [Manager Name],
+
+  I hope this email finds you well. I wanted to share my workplace passport with 
+  you to help us work together more effectively.
+
+  [This would be populated with user's responses]
+
+  Best regards,
+  [User Name]`;
+}
