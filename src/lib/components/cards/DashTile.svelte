888aad856bf7779b522b5d1ca2974dfd852c0405
Jason Warren <jason@foundersandcoders.com>
2025-06-24 14:48:34 +0100

    feat(components): :construction: create skeleton DashTile component

---
+++ src/lib/components/cards/DashTile.svelte
@@ -0,0 +1,13 @@
+<div class="card-container">
+  <div class="card-header">
+    <h2 class="card-title">
+        Card Title
+      </h2>
+  </div>
+
+  <div class="card-content">
+    <p>
+      Card Content
+    </p>
+  </div>
+</div>
